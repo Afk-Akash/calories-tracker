3f085597d4b210ee9d9fb0f289f28719c934b682
Akash Kumar Singh <ak9072918@gmail.com>
2024-10-13 15:51:07 +0530

    update ingredients/meal model

--- models/dailyLogs.go
+++ models/dailyLogs.go
@@ -3,10 +3,13 @@ package models
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type DailyLog struct {
-    ID           primitive.ObjectID `bson:"_id,omitempty" json:"id,omitempty"`
-    UserID       primitive.ObjectID `bson:"user_id" json:"user_id"`
-    Date         primitive.DateTime `bson:"date" json:"date"`
-    Meals        []Meal             `bson:"meals" json:"meals"`
-    TotalCalories float64           `bson:"total_calories" json:"total_calories"`
-    CreatedAt    primitive.DateTime `bson:"created_at" json:"created_at"`
+    ID            primitive.ObjectID `bson:"_id,omitempty" json:"id,omitempty"`
+    UserID        primitive.ObjectID `bson:"user_id" json:"user_id"`
+    Date          primitive.DateTime `bson:"date" json:"date"`
+    Meals         []Meal             `bson:"meals" json:"meals"`
+    TotalCalories float64            `bson:"total_calories" json:"total_calories"`
+    TotalProtien  float64            `bson:"total_protien" json:"total_protien"`
+    TotalCarbs    float64            `bson:"total_carbs" json:"total_carbs"`
+    TotalFat      float64            `bson:"total_fat" json:"total_fat"`
+    CreatedAt     primitive.DateTime `bson:"created_at" json:"created_at"`
 }
--- models/ingredients.go
+++ models/ingredients.go
@@ -10,9 +10,9 @@ type Ingredient struct {
     ID        primitive.ObjectID `bson:"_id,omitempty" json:"id,omitempty"`
     UserID    primitive.ObjectID `bson:"user_id" json:"user_id"`
     Name      string             `bson:"name" json:"name"`
-    Protein float64              `bson:"protein" json:"protein"`
-    Carbs   float64              `bson:"carbs" json:"carbs"`
-    Fat    float64               `bson:"fat" json:"fat"`
+    Protein   float64            `bson:"protein" json:"protein"`
+    Carbs     float64            `bson:"carbs" json:"carbs"`
+    Fat       float64            `bson:"fat" json:"fat"`
     Calories  float64            `bson:"calories" json:"calories"`
     CreatedAt primitive.DateTime `bson:"created_at" json:"created_at"`
 }
